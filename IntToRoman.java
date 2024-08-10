2e79c3988f3c4ec8f588bdcca9dedacf8bf979d6
Shubhajit <shubjitgithub@gmail.com>
2024-08-10 19:53:46 +0530

    LeetCode

--- IntToRoman.java
+++ IntToRoman.java
@@ -13,3 +13,29 @@
 // Note: 49 is not 1 (I) less of 50 (L) because the conversion is based on decimal places
 
 
+
+class IntToRoman {
+    
+    public static void inttoroman(int num) {
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] words = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num = num - values[i];
+                roman.append(words[i]);
+            }
+        }
+    System.out.println(roman);
+}
+
+     public static void main(String[] args) {
+
+inttoroman(3749);
+
+
+     }
+    
+
+    
+}
