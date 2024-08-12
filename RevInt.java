9b0189bbb776c164d6ec8342b563a064487f6d06
Shubhajit <shubjitgithub@gmail.com>
2024-08-12 19:36:34 +0530

    LeetCode

---
+++ RevInt.java
@@ -0,0 +1,18 @@
+
+// Input: x = 123
+// Output: 321
+
+public class RevInt {
+    public static void main(String[] args) {
+        int x = 123;
+        int reverse=0;
+        while (x > 0) {
+            int rem = x % 10;
+            reverse = (reverse * 10) + rem;
+            x = x / 10;
+
+        }
+        System.out.println(reverse);
+    }
+    
+}
