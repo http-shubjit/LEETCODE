ed9499332afa523b373fe2c85da398c94621d85f
Shubhajit <shubjitgithub@gmail.com>
2024-08-27 21:00:19 +0530

    LeetCode

--- Sorting.java
+++ Sorting.java
@@ -104,15 +104,69 @@ private void swap(int[] arr, int i, int j) {
     
    
 
-
-
-
-
 class MergeSort {
 
-    public void Sort(int[] arr) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Sort'");
+    public void Sort(int[] arr, int l, int r) {
+        if (l < r) {
+
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            Sort(arr, l, m);
+            Sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+   
+    static void merge(int arr[], int l, int m, int r){
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+
+        // Initial indices of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
 
@@ -150,11 +204,11 @@ public class Sorting {
         // for (int i = 0; i < sortinser.length; i++) {
         // System.out.println(sortinser[i]);
         // }
-       int[] sortquick=  quicksort.Sort(arr, 0, arr.length - 1);
-       for (int i = 0; i < sortquick.length; i++) {
-       System.out.println(sortquick[i]);
-       } 
-        // mergesort.Sort(arr);
+    //    int[] sortquick=  quicksort.Sort(arr, 0, arr.length - 1);
+    //    for (int i = 0; i < sortquick.length; i++) {
+    //    System.out.println(sortquick[i]);
+    //    } 
+        mergesort.Sort(arr,0,arr.length-1);
 
 }
 }
