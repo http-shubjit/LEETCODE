57f0391930bc21873f4c1d69d614cce1daf6a9ce
Shubhajit <shubjitgithub@gmail.com>
2024-08-28 20:19:52 +0530

    Exact Square Root

--- SquareRoot.java
+++ SquareRoot.java
@@ -5,11 +5,28 @@ public class SquareRoot {
     public static void main(String[] args) {
         int num = 90;
         int res = findSquareRoot(num);
-        System.out.println(res);
+        double exactres = findExactSquareRoot(num, res);
+        System.out.println(exactres);
         
     }
 
-     static int findSquareRoot(int num) {
+    private static double findExactSquareRoot(int num, int res) {
+        double factor = 1;
+        double exactresult = res;
+        for (int i = 0; i < 2; i++) {
+            factor = factor / 10;
+            for (double j = res; j * j < num; j = j + factor) {
+                exactresult = j;
+                
+
+            }
+
+        }
+        return exactresult;
+        
+    }
+
+    static int findSquareRoot(int num) {
         if (num == 0 || num == 1) {
             return num; 
         }
