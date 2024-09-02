50193f214aaa8d3b33bbd955f45bd94ec41f0ccf
Shubhajit <shubjitgithub@gmail.com>
2024-09-02 20:12:36 +0530

    Gcd

--- CodeHelp-DSA Series/Lecture24.java
+++ CodeHelp-DSA Series/Lecture24.java
@@ -1,28 +1,77 @@
 
-//**finding prime no using Sieve of Eratosthenes
+// //**finding prime no using Sieve of Eratosthenes
 
-public class Lecture24 {
-    public static void main(String[] args) {
-        int n = 40;
-        int cnt = 0;
-        boolean[] prime = new boolean[n + 1]; 
-        for (int i = 0; i <prime.length; i++) {
-            prime[i] = true;
+// public class Lecture24 {
+//     public static void main(String[] args) {
+//         int n = 40;
+//         int cnt = 0;
+//         boolean[] prime = new boolean[n + 1]; 
+//         for (int i = 0; i <prime.length; i++) {
+//             prime[i] = true;
          
-        }
-        prime[0] = false;
-        prime[1]=false;
-        for (int i = 2; i < prime.length; i++) {
-            if (prime[i])
-         {
-cnt++;
-for (int j = i * 2; j < prime.length; j = j + i)
-                    prime[j] = false;
-            }
+//         }
+//         prime[0] = false;
+//         prime[1]=false;
+//         for (int i = 2; i < prime.length; i++) {
+//             if (prime[i])
+//          {
+// cnt++;
+// for (int j = i * 2; j < prime.length; j = j + i)
+//                     prime[j] = false;
+//             }
 
-        }
-        System.out.println(cnt);
+//         }
+//         System.out.println(cnt);
 
-    }
+//     }
 
-}
+// }
+
+//** 2: Sieve Segment for find find above 10^6 
+
+
+//** 3 :Gcd Euclid’s Algo 
+
+// public class Lecture24 {
+
+//     public static void main(String[] args) {
+        
+//         int a = 12;
+//         int b = 24;
+//         int res = gcd(a, b);
+//         System.out.println(a);
+//     }
+
+//     private static int gcd(int a, int b) {
+//         if(a==0)
+//             return a;
+//         if(b==0)
+//             return b;
+//         while (a != b) {
+//             if (a > b)
+//                 a = a - b;
+//             if (b > a)
+//                 b =b-a;
+//         }
+//         return a;
+//     }
+// }
+
+//** 4:Lcm
+
+
+// public class Lecture24 {
+
+//     public static void main(String[] args) {
+//         int a = 20;
+//         int b = 10;
+//         int lcm=a>b?a:b;
+//         while ( lcm%a != 0 || lcm%b != 0) {
+//             lcm++;
+//         }
+//         System.out.println(lcm);
+        
+//     }
+// }
+
+//** 5: Modular Arithmetics/ Fast Exponentiation
