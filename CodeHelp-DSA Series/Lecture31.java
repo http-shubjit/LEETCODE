c93b19521246911d9be805faa9531392e9d1b5d9
Shubhajit <shubjitgithub@gmail.com>
2024-09-03 19:14:37 +0530

    print 0-n ussing recurssion

--- CodeHelp-DSA Series/Lecture31.java
+++ CodeHelp-DSA Series/Lecture31.java
@@ -43,10 +43,24 @@
 
 //** 3: print 0-n ussing recurssion
 
-/**
- * Lecture31
- */
-public class Lecture31 {
 
+public class Lecture31 {
+    public static void main(String[] args) {
+    
+        int n = 5;
+        print(n);
+}
+
+private static void print(int n) {
+        if(n<0)
+        return;
+        else
+        {
+        System.out.println(n);
+        n--;
+        print(n);
+        }   
+        
+    }
     
 }
