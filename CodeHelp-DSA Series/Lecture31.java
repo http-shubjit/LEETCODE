b2e89f9703ddb9acf60c881055917cfe4c6a4f0c
Shubhajit <shubjitgithub@gmail.com>
2024-09-03 19:12:21 +0530

    x^n using recurssiom

--- CodeHelp-DSA Series/Lecture31.java
+++ CodeHelp-DSA Series/Lecture31.java
@@ -20,3 +20,33 @@
 // }
 
 
+//** 2: x^n
+
+
+// public class Lecture31 {
+
+//     public static void main(String[] args) {
+        
+//         int res = square(5, 3);
+//         System.out.println(res);
+        
+//     }
+
+//     private static int square(int x, int n) {
+        
+//         if(n==0)
+//             return 1;
+        
+//         return x * square(x, n - 1);
+//     }
+// }
+
+//** 3: print 0-n ussing recurssion
+
+/**
+ * Lecture31
+ */
+public class Lecture31 {
+
+    
+}
